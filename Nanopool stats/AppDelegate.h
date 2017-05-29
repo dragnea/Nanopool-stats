a24e3d14e70406644b94c95e1d2199a8cf0169ec
Mihai Dragnea <mihaidragnea@Mihais-Mac-mini.local>
2017-05-29 15:54:52 +0300

    Initial commit

---
+++ Nanopool stats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nanopool stats
+//
+//  Created by Mihai Dragnea on 5/29/17.
+//  Copyright © 2017 Dragnea Mihai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
